19aabc44850607153f50097186c691abdd4b066b
Sébastien Lucas <sebastien@slucas.fr>
2017-12-29 14:45:19 +0100

    Don't load all CSS directly in the HTML.
    Instead load it in an external file generated by gulp
    Chroma_native was change to its full version as everything is minified later

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+
+var gulp = require('gulp');
+
+// Import dependencies
+var concat   = require('gulp-concat');
+var hash     = require ('gulp-hash');
+var cleanCSS = require('gulp-clean-css');
+var del      = require('del');
+
+var source = 'themes/cocoa-eh/layouts/partials/css/';
+
+var cssSources = [source + 'main.css',
+                  source + 'min600px.css',
+                  source + 'min769px.css',
+                  source + 'chroma_native.css'];
+
+var datadir = 'themes/cocoa-eh/data';
+var data = {
+css: datadir + '/css/'
+};
+
+var publishdir = 'themes/cocoa-eh/static';
+var dist = {
+css: publishdir + '/css/'
+};
+// Define tasks
+
+gulp.task('css', function() {
+  del(dist.css + '*')
+  return gulp.src(cssSources)
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(concat('site.min.css'))
+      .pipe(hash())
+      .pipe(gulp.dest(dist.css))
+      .pipe(hash.manifest('hash.json'))
+      .pipe(gulp.dest(data.css));
+});
+
+gulp.task('default', ['css']);
